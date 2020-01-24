f5615daa2a5b62a4b53ed9f71495b88c52834fb1
Paul Pan <panjunhong05@gmail.com>
2020-01-23 17:30:40 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Paul Pan on 20/23/1.
+//  Copyright © 2020 Paul Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
